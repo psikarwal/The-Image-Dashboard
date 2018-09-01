b99cd1411d0233bc852a25893a3ebefc7c97174a
Piyush Sikarwal <sikarwal.ps@gmail.com>
2018-09-01 09:47:51 +0530

    Removed Dead code

--- src/App.js
+++ src/App.js
@@ -7,16 +7,7 @@ import EditImage from './containers/EditImage';
 import './App.css';
 
 class App extends Component {
-  state = { photos: [] };
   render() {
-    const handleFileUpload = e => {
-      const file = Array.from(e.target.files);
-      this.setState({ photos: file });
-      console.log(8, file);
-      console.log(9, e.target);
-    };
-    console.log(14, this.state);
-
     return (
       <div className="App">
         <Header />
