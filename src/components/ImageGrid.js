969a7a1d276e4f1a72d76b628bab42bf0343c35d
Piyush Sikarwal <sikarwal.ps@gmail.com>
2018-09-01 09:37:37 +0530

    Refined ImageGrid

--- src/components/ImageGrid.js
+++ src/components/ImageGrid.js
@@ -1,5 +1,5 @@
-import React, { Component } from 'react';
-import { withStyles, Paper } from '@material-ui/core';
+import React from 'react';
+import { withStyles } from '@material-ui/core';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
 import CardActions from '@material-ui/core/CardActions';
@@ -7,7 +7,8 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import cx from 'classnames';
+import { Link } from 'react-router-dom';
+import _ from 'lodash';
 
 const styles = {
   standardRow: {
@@ -26,44 +27,65 @@ const styles = {
     display: 'flex',
     flexDirection: 'column',
     marginTop: '5%',
-    marginLeft: '1%'
+    marginLeft: '2%',
+    marginBottom: '2%'
   },
   media: {
     height: 200
   }
 };
 const ImageGrid = props => {
-  const { images = [], classes = {} } = props;
+  const { img = {}, classes = {}, deleteImage = () => {} } = props;
+  console.log(36, props);
+
+  if (_.isEmpty(img)) return <React.Fragment />;
   return (
-    <div className={cx(classes.standardRow, classes.wrap, classes.spaceEven)}>
-      {images.map(img => (
-        <Card elevation={5} className={classes.card}>
-          <CardActionArea>
-            <CardMedia
-              className={classes.media}
-              image={img}
-              title="Contemplative Reptile"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="headline" component="h2">
-                Name
-              </Typography>
-              <Typography component="p">Resolution</Typography>
-            </CardContent>
-          </CardActionArea>
-          <CardActions
-            style={{ display: 'flex', flexDirection: 'row-reverse' }}
+    <Card elevation={5} className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={img.src}
+          title="Contemplative Reptile"
+        />
+        <CardContent>
+          <Typography
+            style={{
+              whiteSpace: 'nowrap',
+              overflow: 'hidden',
+              textOverflow: 'ellipsis'
+            }}
+            gutterBottom
+            variant="headline"
+            component="h2"
           >
-            <Button size="small" color="primary">
-              Edit
-            </Button>
-            <Button size="small" color="secondary">
-              Delete
-            </Button>
-          </CardActions>
-        </Card>
-      ))}
-    </div>
+            {img.name}
+          </Typography>
+          <Typography style={{ textTransform: 'uppercase' }} component="p">{`${
+            img.extension
+          } - ${img.resolution}`}</Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions style={{ display: 'flex', flexDirection: 'row-reverse' }}>
+        <Link
+          style={{ textDecoration: 'none' }}
+          to={`/editPost/${img.name.charAt(0).toUpperCase() +
+            img.name.substr(1)}`}
+        >
+          <Button size="small" color="primary">
+            Edit
+          </Button>
+        </Link>
+        <Button
+          onClick={() =>
+            deleteImage(img.name.charAt(0).toUpperCase() + img.name.substr(1))
+          }
+          size="small"
+          color="secondary"
+        >
+          Delete
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
